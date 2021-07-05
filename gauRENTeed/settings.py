419e1e3d0af823d338c5a14c27499b163d03cc8f
Oz Kilim <ozkilim@hotmail.co.uk>
2021-07-05 13:46:21 +0100

    apps

--- gauRENTeed/settings.py
+++ gauRENTeed/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['shrouded-plateau-92946.herokuapp.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'formtools',
-    'django.contrib.sites'
     'beta',
 ]
 
