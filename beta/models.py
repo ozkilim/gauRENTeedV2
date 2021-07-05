6fddcdac906a87b0163afeb3e70b857f1f1533ee
Oz Kilim <ozkilim@hotmail.co.uk>
2021-07-05 15:03:15 +0100

    LONGER FIELDSS

--- beta/models.py
+++ beta/models.py
@@ -31,10 +31,10 @@ class CustomUser(AbstractUser):
 
 class Property(models.Model):
     fullAddress = models.CharField(
-        unique=True, max_length=255, default="", blank=True)
+        unique=True, max_length=500, default="", blank=True)
 
     postcode = models.CharField(
-        unique=False, max_length=255, default="", blank=True)
+        unique=False, max_length=500, default="", blank=True)
 
     hashId = models.UUIDField(default=uuid.uuid4, editable=False)
 
@@ -60,7 +60,7 @@ class Review(models.Model):
         (1, 1), (2, 2,), (3, 3), (4, 4), (5, 5)
     ]
     timeStamp = models.CharField(
-        null=True, blank=True, help_text='Time data was collected', max_length=255)
+        null=True, blank=True, help_text='Time data was collected', max_length=500)
 
     livingConfirmation = models.BooleanField(default=True, blank=True,
                                              help_text='Do you confirm that you live or lived at the property in question?')
@@ -81,65 +81,66 @@ class Review(models.Model):
         ("Student", "Student"), ("Employed", "Employed"), ("Self-Employed", "Self-Employed")]
 
     employmentStatus = models.CharField(null=True,
-                                        choices=USER_OCCUPATION_CHOICES, default="Student", max_length=255
+                                        choices=USER_OCCUPATION_CHOICES, default="Student", max_length=500
                                         )
 
-    reviewerName = models.CharField(max_length=255, null=True)
+    reviewerName = models.CharField(max_length=500, null=True)
 
     buildingQuality = models.IntegerField(null=True, blank=True,
                                           choices=RATING_CHOICES,
                                           default=3, help_text='How would you rate the quality of the building (considering mould, pests, cleanliness, damp, etc.)?')
 
     buildigComment = models.CharField(
-        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=255)
+        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=500)
 
     moveInHygene = models.IntegerField(null=True, blank=True,
                                        choices=RATING_CHOICES,
                                        default=3, help_text='When you moved in, how clean was the property?')
 
     moveInHygeneComment = models.CharField(
-        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=255)
+        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=500)
 
     utilities = models.IntegerField(null=True, blank=True,
                                     choices=RATING_CHOICES,
                                     default=3, help_text='How would you rate the utilities? (water pressure, heating, insulation etc)')
 
     utilitiesComment = models.CharField(
-        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=255)
+        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=500)
 
     bedroomQuality = models.IntegerField(null=True, blank=True,
                                          choices=RATING_CHOICES,
                                          default=3, help_text='What is the quality of the bedrooms at the property? (Size, comfort, privacy etc)')
 
     bedroomQualityComment = models.CharField(
-        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=255)
+        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=500)
 
     furnishings = models.IntegerField(null=True, blank=True,
                                       choices=RATING_CHOICES,
                                       default=3, help_text='If the property was furnished, what is the quality of the furnishings like? (Beds, wardrobes, tables, sofas, etc.)')
 
     furnishingsComment = models.CharField(
-        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=255)
+        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=500)
 
     manageResponsivenes = models.IntegerField(null=True, blank=True,
                                               choices=RATING_CHOICES,
                                               default=3, help_text='When you have a problem how responsive is the property manager? ')
 
     manageResponsivenesComment = models.CharField(
-        null=True, blank=True, help_text='Can you explain why you have given them the rating that you have?', max_length=255)
+        null=True, blank=True, help_text='Can you explain why you have given them the rating that you have?', max_length=500)
 
     repairQuality = models.IntegerField(null=True, blank=True,
                                         choices=RATING_CHOICES,
                                         default=3, help_text='When the property manager arranges a repair, the repair is usually...')
 
     repairQualityComment = models.CharField(
-        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=255)
+        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=500)
 
     hiddenExpenses = models.CharField(
-        null=True, blank=True, help_text='Were there any hidden expenses at the end of the tenancy? (Cleaning costs, significant deposit reductions, unexpected charges, etc) ', max_length=255)
+        null=True, blank=True, help_text='Were there any hidden expenses at the end of the tenancy? (Cleaning costs, significant deposit reductions, unexpected charges, etc) ', max_length=500)
 
     wantedToKnowBefore = models.CharField(
-        null=True, blank=True, help_text='Is there anything else you would have liked to have known about the property manager before you moved in?', max_length=255)
+        null=True, blank=True, help_text='Is there anything else you would have liked to have known about the property manager before you moved in?', max_length=500
+    )
 
     rentMonthly = models.PositiveIntegerField(null=True,
                                               default=500, help_text='What is the total monthly rent for the property?')
@@ -148,14 +149,14 @@ class Review(models.Model):
                                        help_text='Do you feel the total monthly rent is a good deal?')
 
     neighbourhoodDescription = models.CharField(
-        null=True, blank=True, help_text='How would you describe the neighbourhood to someone thinking of living there?', max_length=255)
+        null=True, blank=True, help_text='How would you describe the neighbourhood to someone thinking of living there?', max_length=500)
 
     neighbourhoodSafety = models.IntegerField(null=True, blank=True,
                                               choices=RATING_CHOICES,
                                               default=3, help_text='How safe do you feel in the neighbourhood?')
 
     neighbourhoodSafetyComment = models.CharField(
-        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=255)
+        null=True, blank=True, help_text='Can you explain why you have given it the rating that you have?', max_length=500)
 
     neighbourhoodEnjoyment = models.IntegerField(null=True, blank=True,
                                                  choices=RATING_CHOICES,
@@ -210,10 +211,10 @@ class Review(models.Model):
                                     default=3, help_text='I was able to have a hot shower whenever I wanted')
 
     likedMost = models.CharField(
-        null=True, blank=True, help_text='What do/did you like most about the property?', max_length=255)
+        null=True, blank=True, help_text='What do/did you like most about the property?', max_length=500)
 
     likedLeast = models.CharField(
-        null=True, blank=True, help_text='What do/did you like least about the property?', max_length=255)
+        null=True, blank=True, help_text='What do/did you like least about the property?', max_length=500)
 
     overallRating = models.IntegerField(null=True, blank=True,
                                         choices=RATING_CHOICES,
@@ -229,13 +230,13 @@ class Review(models.Model):
                                         help_text='Would you like to hear about future opportunities to work as a paid ambassador for GuaRENTeed?')
 
     email = models.CharField(
-        null=True, blank=True, help_text='email', max_length=255)
+        null=True, blank=True, help_text='email', max_length=500)
 
     ambassadorPotential = models.BooleanField(default=True, null=True,
                                               help_text='Would you like to be sent paid opportunities to be a GuaRENTeed ambassador?')
 
     whoSentSurvey = models.CharField(
-        null=True, blank=True, help_text='Who sent you this survey? (Leave blank if you scanned the QR code on our leaflet)', max_length=255)
+        null=True, blank=True, help_text='Who sent you this survey? (Leave blank if you scanned the QR code on our leaflet)', max_length=500)
 
     maintenanceMoveIn = models.IntegerField(null=True, blank=True,
                                             choices=RATING_CHOICES,
@@ -246,20 +247,20 @@ class Review(models.Model):
                                      default=3, help_text='MAINTENANCE/CONDITION: What was the quality of the white goods (dishwasher, washing machine etc)')
 
     qualityComments = models.CharField(
-        null=True, blank=True, help_text='Any other comment about the quality of property?', max_length=255)
+        null=True, blank=True, help_text='Any other comment about the quality of property?', max_length=500)
 
     landlordRating = models.IntegerField(null=True, blank=True,
                                          choices=RATING_CHOICES,
                                          default=3, help_text='LANDLORD: Any other comments')
 
     landlordComments = models.CharField(
-        null=True, blank=True, help_text='LANDLORD: Any other comments', max_length=255)
+        null=True, blank=True, help_text='LANDLORD: Any other comments', max_length=500)
 
     areaBenefits = models.CharField(
-        null=True, blank=True, help_text='What were the benefits of the area?', max_length=255)
+        null=True, blank=True, help_text='What were the benefits of the area?', max_length=500)
 
     areaPerks = models.CharField(
-        null=True, blank=True, help_text='Perks of area', max_length=255)
+        null=True, blank=True, help_text='Perks of area', max_length=500)
 
     wouldRecommendProperty = models.BooleanField(default=True, null=True,
                                                  help_text='Would you reccomend this property to others? ')
