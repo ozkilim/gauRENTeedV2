efa05df08a37215ec6444773a50c7149321c6bd2
Oz Kilim <ozkilim@hotmail.co.uk>
2021-07-05 11:15:03 +0100

    allowdHosts

--- gauRENTeed/settings.py
+++ gauRENTeed/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '6dnm)#uud6@gaxe+fh%ke=a!_rfe-h)e0z^7ty^^imiugzqsk%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['morning-anchorage-49744.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['shrouded-plateau-92946.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
