81845c23c6c291bcb7d9e4376645679eb5b944f8
Oz Kilim <ozkilim@hotmail.co.uk>
2021-08-05 09:44:15 +0200

    email key in heroku

--- gauRENTeed/settings.py
+++ gauRENTeed/settings.py
@@ -240,9 +240,9 @@ STATIC_URL = '/static/'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ozsamkilim@gmail.com'
-EMAIL_HOST_PASSWORD = 'monkey/218'
+EMAIL_HOST_USER = 'guarenteedteam@gmail.com'
+
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 EMAIL_USE_TLS = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'
-# LOGIN_REDIRECT_URL = 'landing'
-# LOGOUT_REDIRECT_URL = 'landing'
