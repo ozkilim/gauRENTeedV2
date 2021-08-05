55af566051e48a9101b1ca854a3eae1ee65ef21b
Oz Kilim <ozkilim@hotmail.co.uk>
2021-08-05 09:18:40 +0200

    static files

--- gauRENTeed/settings.py
+++ gauRENTeed/settings.py
@@ -189,10 +189,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
+    os.path.join(os.path.normpath(BASE_DIR), "beta/static"),
 
-    os.path.join(BASE_DIR, '/static/'),
-
-    os.path.join(BASE_DIR, '/media/'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
